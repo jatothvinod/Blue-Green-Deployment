1df431524782e52ebd7873b053bfcf2ce1567075
Jatoth Vinod Naik <141837006+jatothvinod@users.noreply.github.com>
2025-02-11 15:48:47 +0530

    Update variables.tf

--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "edbcf7KXrN9duPZ780TE8fAKZtIOhrEkclcyHVgK"
+  default     = "BGkey.pem"
 }
 
