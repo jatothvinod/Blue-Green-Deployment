5cf8c0a889af94fdb9c950f1df338f543f552913
Jatoth Vinod Naik <141837006+jatothvinod@users.noreply.github.com>
2025-02-11 15:18:38 +0530

    Update variables.tf

--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "DevOps-Shack"
+  default     = "edbcf7KXrN9duPZ780TE8fAKZtIOhrEkclcyHVgK"
 }
 
